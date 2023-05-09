55759602ae2b0d3ce645874336ccd5158f199487
choigeon96 <ck731@naver.com>
2023-05-09 22:29:47 +0900

    refactor: drawFrame() video가 아닌 src에도 사용할 수 있도록 수정

--- seed/src/components/ReactiveCanvas/index.tsx
+++ seed/src/components/ReactiveCanvas/index.tsx
@@ -1,14 +1,20 @@
+import { useAnimation } from "#/hooks/useAnimation";
 import useForwardRef from "#/hooks/useForwardRef";
 import styled from "@emotion/styled";
 import { forwardRef, useEffect } from "react";
+
+interface ReactiveCanvasProps{
+  
+}
 
 const Canvas = styled.canvas`
   width: 100%;
   height: 100%;
 `;
 
-const ReactiveCanvas = forwardRef<HTMLCanvasElement>((_, ref) => {
+const ReactiveCanvas = forwardRef<HTMLCanvasElement, ReactiveCanvasProps>((props, ref) => {
   const canvasRef = useForwardRef<HTMLCanvasElement>(ref, null);
+  const { drawFrame } = useAnimation();
   useEffect(() => {
     const canvas = canvasRef.current;
     [canvas.width, canvas.height] = [canvas.offsetWidth, canvas.offsetHeight];
--- seed/src/hooks/useAnimation.ts
+++ seed/src/hooks/useAnimation.ts
@@ -2,13 +2,12 @@ import { useCallback } from "react";
 
 export const useAnimation = () => {
   const drawFrame = useCallback(
-    (video: HTMLVideoElement, canvas: HTMLCanvasElement) =>
+    (src: CanvasImageSource, canvas: HTMLCanvasElement) =>
       requestAnimationFrame(function drawFrame() {
-        const [width, height] = [video.videoWidth, video.videoHeight];
         //drawImage 매개변수 관련 예전에 리뷰 받았던 PR 참조 https://github.com/Themion/javascript-p4-bmtown/pull/10
         canvas
           .getContext("2d")
-          ?.drawImage(video, 0, 0, width, height, 0, 0, canvas.width, canvas.height);
+          ?.drawImage(src, 0, 0, canvas.width, canvas.height);
         requestAnimationFrame(drawFrame);
       }),
     []
