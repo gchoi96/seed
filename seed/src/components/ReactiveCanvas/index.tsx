477cde5702c7a66c86cc1caa3148390535c65f42
choigeon96 <ck731@naver.com>
2023-05-15 03:29:29 +0900

    tesseract test

--- seed/src/components/ReactiveCanvas/index.tsx
+++ seed/src/components/ReactiveCanvas/index.tsx
@@ -3,18 +3,13 @@ import useForwardRef from "#/hooks/useForwardRef";
 import styled from "@emotion/styled";
 import { forwardRef, useEffect } from "react";
 
-interface ReactiveCanvasProps{
-  
-}
-
 const Canvas = styled.canvas`
   width: 100%;
   height: 100%;
 `;
 
-const ReactiveCanvas = forwardRef<HTMLCanvasElement, ReactiveCanvasProps>((props, ref) => {
+const ReactiveCanvas = forwardRef<HTMLCanvasElement>((_, ref) => {
   const canvasRef = useForwardRef<HTMLCanvasElement>(ref, null);
-  const { drawFrame } = useAnimation();
   useEffect(() => {
     const canvas = canvasRef.current;
     [canvas.width, canvas.height] = [canvas.offsetWidth, canvas.offsetHeight];
--- seed/src/pages/capturetest/index.tsx
+++ seed/src/pages/capturetest/index.tsx
@@ -2,6 +2,7 @@ import { useScreenCapture } from "#/hooks/useScreenCapture";
 import { useEffect, useRef } from "react";
 import { useAnimation } from "#/hooks/useAnimation";
 import ReactiveCanvas from "#/components/ReactiveCanvas";
+import Tesseract from "tesseract.js";
 export default function CaptureTest() {
   const { selectScreen, videoRef } = useScreenCapture();
   const { drawFrame } = useAnimation();
@@ -12,10 +13,16 @@ export default function CaptureTest() {
     drawFrame(videoRef.current, canvasRef.current);
   }, [drawFrame, videoRef, canvasRef]);
 
+  const onClickOCR = () => {
+    if (!canvasRef.current) return;
+    Tesseract.recognize(canvasRef.current, "kor").then(({ data: { text } }) => console.log(text));
+  };
+
   return (
     <div style={{ width: "100vw", height: "100vh", padding: "10%" }}>
       <ReactiveCanvas ref={canvasRef} />
       <button onClick={selectScreen}>화면 선택</button>
+      <button onClick={onClickOCR}>텍스트 추출</button>
     </div>
   );
 }
