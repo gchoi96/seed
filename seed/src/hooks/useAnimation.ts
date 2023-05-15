674896238c57d9e8a7edbe269bcca52a35019012
choigeon96 <ck731@naver.com>
2023-05-15 15:40:31 +0900

    src crop

--- seed/src/hooks/useAnimation.ts
+++ seed/src/hooks/useAnimation.ts
@@ -5,7 +5,7 @@ export const useAnimation = () => {
     (src: CanvasImageSource, canvas: HTMLCanvasElement) =>
       requestAnimationFrame(function drawFrame() {
         //drawImage 매개변수 관련 예전에 리뷰 받았던 PR 참조 https://github.com/Themion/javascript-p4-bmtown/pull/10
-        canvas.getContext("2d")?.drawImage(src, 0, 0, 100, 100);
+        canvas.getContext("2d")?.drawImage(src, 0, 0, 200, 200, 0, 0, canvas.width, canvas.height);
         requestAnimationFrame(drawFrame);
       }),
     []
